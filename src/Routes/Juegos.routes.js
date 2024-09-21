0da5f733ab932587a5f0dcad5a29eb084ed44355
HatCodeDev <misael.reynoso.aguayo@gmail.com>
2024-09-20 21:17:42 -0600

    feat(Autenticacion): Autenticacion JWT y corrección de errores

--- src/Routes/Juegos.routes.js
+++ src/Routes/Juegos.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { getJuegos, saveJuego, updateJuego, deleteJuego } from '../Controllers/JuegosController.js';
 import { subirImagen } from "../Middleware/Storage.js";
-
+import { verificar } from "../Middleware/Auth.js";
 
 const rutas = Router()
 
-rutas.get('/api/juegos', getJuegos)
-rutas.get('/api/juegos/:id', getJuegos)
-rutas.post('/api/juegos', subirImagen.single('imagen'), saveJuego)
-rutas.put('/api/juegos/:id', subirImagen.single('imagen'), updateJuego)
-rutas.delete('/api/juegos/:id', deleteJuego)
+rutas.get('/api/juegos',verificar, getJuegos)
+rutas.get('/api/juegos/:id',verificar, getJuegos)
+rutas.post('/api/juegos',verificar, subirImagen.single('imagen'), saveJuego)
+rutas.put('/api/juegos/:id',verificar, subirImagen.single('imagen'), updateJuego)
+rutas.delete('/api/juegos/:id',verificar, deleteJuego)
 
 
 export default rutas
